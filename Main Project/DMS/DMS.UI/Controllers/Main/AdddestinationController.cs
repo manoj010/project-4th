0789c7f2dd0759d6ebf27e07c9a77da514e8f929
manoj010 <manojale100@gmail.com>
2022-07-16 15:08:53 +0545

    update

---
+++ Main Project/DMS/DMS.UI/Controllers/Main/AdddestinationController.cs
@@ -0,0 +1,72 @@
+﻿using DMS.DAL.DatabaseContext;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS.Controllers.Main
+{
+    public class AdddestinationController : Controller
+    {
+        MainEntities db = new MainEntities();
+        // GET: Adddestination
+        public ActionResult index()
+        {
+            List<destination> all_data = db.destinations.ToList();
+            return View(all_data);
+
+        }
+
+        public ActionResult items()
+        {
+            List<destination> all_data = db.destinations.ToList();
+            return View(all_data);
+        }
+
+
+        public ActionResult create()
+        {
+
+            return View();
+        }
+        public ActionResult Savedata(destination destination, HttpPostedFileBase photo)
+        {
+            string path = Server.MapPath("~/Uploads");
+            string filename = photo.FileName;
+            string new_path = path + "/" + filename;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            photo.SaveAs(new_path);
+            destination.photo = "~/Uploads";
+            destination.photo_name = filename;
+            db.destinations.Add(destination);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public ActionResult edit(int id)
+        {
+            destination destination = db.destinations.Find(id);
+            //employee data=db.employees.firstordefault(x=>x.id==id);
+            return View(destination);
+        }
+        public ActionResult Updatedata(destination destination)
+        {
+
+            db.Entry(destination).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public ActionResult deletedata(int id)
+        {
+            destination data = db.destinations.Find(id);
+            db.destinations.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ Main Project/DMS/DMS.UI/Controllers/Main/GalleryController.cs
@@ -0,0 +1,52 @@
+﻿using DMS.DAL.DatabaseContext;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS.Controllers.Main
+{
+    public class GalleryController : Controller
+    {
+        MainEntities db = new MainEntities();
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            List<gallery> data = db.galleries.ToList();
+            return View(data);
+        }
+
+        public ActionResult AddNew()
+        {
+            return View();
+        }
+
+        public ActionResult SaveImage(HttpPostedFileBase SelectedFile)
+        {
+            string path = Server.MapPath("~/Uploads");
+            string filename = SelectedFile.FileName;
+            string new_path = path + "/" + filename;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            SelectedFile.SaveAs(new_path);
+            gallery gallery = new gallery();
+            gallery.photo_path = "~/ Uploads";
+            gallery.destination_name = filename;
+            db.galleries.Add(gallery);
+            db.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Search(string name1)
+        {
+            var data1 = db.galleries.Where(x => x.destination_name == name1).ToList();
+            return View("gallery", data1);
+        }
+    }
+}
